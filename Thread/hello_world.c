f9c1568d559e77f7b86eb8f44f3586a5d79b6676
chuyalvarez <jesusaagomez@hotmail.com>
2017-10-04 19:22:54 -0500

    managing race condition through semaphores

--- Thread/hello_world.c
+++ Thread/hello_world.c
@@ -1,12 +1,24 @@
 #include <pthread.h>
  #include <stdio.h>
- #define NUM_THREADS     1000
+ #include <unistd.h>
+ #include <semaphore.h>
 
+ #define NUM_THREADS     10
+
+ long saldo;
+ sem_t mutex;
  void *PrintHello(void *threadid)
  {
     long tid;
+    long lsaldo;
     tid = (long)threadid;
     printf("Hello World! It's me, thread #%ld!\n", tid);
+    sem_wait(&mutex);
+    lsaldo = saldo;
+    sleep(1); // forzando a perder el cpu
+    lsaldo += 1;
+    saldo = lsaldo;
+    sem_post(&mutex);
     pthread_exit(NULL);
  }
 
@@ -15,15 +27,22 @@
     pthread_t threads[NUM_THREADS];
     int rc;
     long t;
+
+    saldo = 0;
+    sem_init(&mutex,0,1);
+
     for(t=0; t<NUM_THREADS; t++){
        printf("In main: creating thread %ld\n", t);
        rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
        if (rc){
           printf("ERROR; return code from pthread_create() is %d\n", rc);
-          exit(-1);
+          //exit(-1);
        }
     }
-
+    for(t=0; t<NUM_THREADS; t++){
+      pthread_join(threads[t], NULL);
+    }
+    printf("El valor del saldo es %ld\n", saldo);
     /* Last thing that main() should do */
     pthread_exit(NULL);
  }
